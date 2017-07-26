0dd4789def0bb4d386bcfb130b1531b25ec81fc0
zhishierge <895990111@qq.com>
2017-07-26 21:19:00 +0800

    被调用端添加支持认证的httpinvoker

---
+++ test-client/src/main/java/org/xyz/test/client/service/IRpcService.java
@@ -0,0 +1,7 @@
+package org.xyz.test.client.service;
+
+/**
+ * Created by yizhenn on 2017/7/26.
+ */
+public class IRpcService {
+}
---
+++ test-service/src/main/java/org/xyz/test/service/securehttpinvoker/SecureHttpInvokerServiceExporter.java
@@ -0,0 +1,9 @@
+package org.xyz.test.service.securehttpinvoker;
+
+import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
+
+/**
+ * Created by yizhenn on 2017/7/26.
+ */
+public class SecureHttpInvokerServiceExporter extends HttpInvokerServiceExporter {
+}
